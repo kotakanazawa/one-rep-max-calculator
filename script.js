6de4d1c3d7cd7e4cbdeeda2313f009e59bd1edfc
kotakanazawa <kota0818@gmail.com>
2019-07-12 11:30:12 +0900

    css修正

--- script.js
+++ script.js
@@ -20,34 +20,34 @@
     if (weight === '' || rep === '') {
       return;
     } else {
-    // 1RM = w * (36/(37-r));
-    const oneRepMax = Math.floor(weight * (1 + rep/40));
-    
-    // 60~95%RMの計算
-    let ary = [];
-    for (var i = 0.6; i <= 1; i = i + 0.05) {
-      const result = Math.floor((i * oneRepMax)); 
-      ary.push(result);
-    }
+      // 1RM = w * (36/(37-r));
+      const oneRepMax = Math.floor(weight * (1 + rep/40));
 
-    // 60~95%RMの計算結果を配列に代入
-    let list = [];
-    for (var i = 60, y = 0; i < 100; i = i + 5, y++) {
-      list.push(`<li>${i}%RM: ${ary[y]}kg</li>`);
-    }
+      // 60~95%RMの計算
+      let ary = [];
+      for (var i = 0.6; i <= 1; i = i + 0.05) {
+        const result = Math.floor((i * oneRepMax)); 
+        ary.push(result);
+      }
 
-    // 計算結果表示
-    removeAllChildren(resultDivided);
-    const paragraphWeight = document.createElement('h2');
-    const paragraphRMs = document.createElement('ul');
+      // 60~95%RMの計算結果を配列に代入
+      let list = [];
+      for (var i = 60, y = 0; i < 100; i = i + 5, y++) {
+        list.push(`<li>${i}%RM: ${ary[y]}kg</li>`);
+      }
 
-    paragraphWeight.classList.add('result-h2');
-    paragraphRMs.classList.add('result-ul');
-    paragraphWeight.innerText = `あなたのマックス重量：${oneRepMax}kg`;
-    paragraphRMs.innerHTML = list.join('');
-    
-    resultDivided.appendChild(paragraphWeight);
-    resultDivided.appendChild(paragraphRMs);
+      // 計算結果表示
+      removeAllChildren(resultDivided);
+      const paragraphWeight = document.createElement('h2');
+      const paragraphRMs = document.createElement('ul');
+
+      paragraphWeight.classList.add('result-h2');
+      paragraphRMs.classList.add('result-ul');
+      paragraphWeight.innerText = `あなたのマックス重量：${oneRepMax}kg`;
+      paragraphRMs.innerHTML = list.join('');
+
+      resultDivided.appendChild(paragraphWeight);
+      resultDivided.appendChild(paragraphRMs);
     }
   }
 
